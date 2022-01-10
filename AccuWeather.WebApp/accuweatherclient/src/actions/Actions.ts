cb5e43a234731a771ac8b7035626815e9fa5936c
htphong24 <htphong24@gmail.com>
2022-01-09 22:17:34 -0800

    Added actions/Actions.ts

---
+++ AccuWeather.WebApp/accuweatherclient/src/actions/Actions.ts
@@ -0,0 +1,52 @@
+﻿import { ThunkDispatch } from 'redux-thunk';
+import axios, { AxiosError } from 'axios';
+import { City } from "../types/City";
+import { CurrentCondition } from "../types/CurrentCondition";
+import { Weather } from "../types/Weather";
+import { AppActions } from "../types/Actions";
+import { Constants } from "../Constants";
+
+export const getCountries = (): AppActions => ({
+    type: "GET_COUNTRIES",
+    payload: {
+        request: {
+            url: `${Constants.locationAPIUrl}/countries?apikey=${Constants.apiKey}`
+        }
+    }
+});
+
+export const getWeather = (countryCode: string, searchText: string) => {
+    return async (dispatch: ThunkDispatch<any, any, AppActions>) => {
+        try {
+            var res = await axios.get(`${Constants.locationAPIUrl}/cities/
+                      ${countryCode}/search?apikey=${Constants.apiKey}&q=${searchText}`);
+            const cities = res.data as City[];
+            var weather = {} as Weather;
+            if (cities.length > 0) {
+                const city = cities[0];
+                weather = await getCurrentWeather(city);
+            }
+
+            return dispatch({
+                type: "GET_WEATHER_SUCCESS",
+                payload: weather
+            });
+        }
+        catch (e: any) {
+            return dispatch({
+                type: "GET_WEATHER_FAIL",
+                error: e.isAxiosError ? e.message : JSON.stringify(e)
+            });
+        }
+    }
+}
+
+export async function getCurrentWeather(city: City): Promise<Weather> {
+    const res = await axios.get(`${Constants.currentConditionsAPIUrl}/
+                               ${city.Key}?apikey=${Constants.apiKey}`);
+    const currentConditions = res.data as CurrentCondition[];
+    if (currentConditions.length > 0) {
+        return new Weather(currentConditions[0], city);
+    }
+    return {} as Weather;
+}
