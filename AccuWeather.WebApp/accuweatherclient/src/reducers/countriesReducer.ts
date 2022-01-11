fa68763c407f715447107b9859e05c340f2db8d2
htphong24 <htphong24@gmail.com>
2022-01-10 18:28:01 -0800

    Added reducers/countriesReducer.ts

---
+++ AccuWeather.WebApp/accuweatherclient/src/reducers/countriesReducer.ts
@@ -0,0 +1,19 @@
+﻿import { Country } from "../types/Country";
+import { AppActions } from "../types/Actions";
+import * as Actions from "../types/Actions";
+
+const countriesReducerDefaultState: Country[] = [];
+
+const countriesReducer = (state = countriesReducerDefaultState, action: AppActions):
+    Country[] => {
+    switch (action.type) {
+        case Actions.GET_COUNTRIES_SUCCESS:
+            const data = action.payload.data;
+            return data as Country[];
+        case Actions.GET_COUNTRIES_FAIL:
+            return state;
+        default:
+            return state;
+    }
+};
+export { countriesReducer };
