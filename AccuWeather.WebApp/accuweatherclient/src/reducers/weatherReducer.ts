5ef84acfec7eee282b3e482d925345cd6f5d068d
htphong24 <htphong24@gmail.com>
2022-01-11 17:53:19 -0800

    Added weatherReducer.ts

---
+++ AccuWeather.WebApp/accuweatherclient/src/reducers/weatherReducer.ts
@@ -0,0 +1,22 @@
+﻿import { Weather } from "../types/Weather";
+import { AppActions } from "../types/Actions";
+import * as Actions from "../types/Actions";
+
+const weatherReducerDefaultState: Weather = {};
+
+const weatherReducer = (state = weatherReducerDefaultState, action: AppActions): Weather => {
+    switch (action.type) {
+        case Actions.GET_WEATHER_SUCCESS:
+            const weather = action.payload;
+            return weather;
+
+        case Actions.GET_WEATHER_FAIL:
+            return {
+                error: action.error
+            } as Weather;
+
+        default:
+            return state;
+    }
+};
+export { weatherReducer };
