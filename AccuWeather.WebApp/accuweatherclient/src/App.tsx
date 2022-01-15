f6c2310ff2ab334443410347b6feec879ea4f066
htphong24 <htphong24@gmail.com>
2022-01-14 18:10:14 -0800

    Added Provider to App.tsx

--- AccuWeather.WebApp/accuweatherclient/src/App.tsx
+++ AccuWeather.WebApp/accuweatherclient/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import "./App.css";
 import AppRouter from "./router";
+import { Provider } from "react-redux";
+import { store } from './store/configureStore';
+
+
 const App: React.FC = () => {
     return (
-        <AppRouter />
+        <Provider store={store}>
+            <AppRouter />
+        </Provider>
     );
 }
 export default App;
