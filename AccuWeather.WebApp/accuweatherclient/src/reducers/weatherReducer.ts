9736f6ffcd4fca0eaf5de3169aeccdfc7100a45b
htphong24 <htphong24@gmail.com>
2022-01-24 18:42:52 -0800

    updated weatherReducer.ts to add an additional city

--- AccuWeather.WebApp/accuweatherclient/src/reducers/weatherReducer.ts
+++ AccuWeather.WebApp/accuweatherclient/src/reducers/weatherReducer.ts
@@ -20,3 +20,20 @@ const weatherReducer = (state = weatherReducerDefaultState, action: AppActions):
     }
 };
 export { weatherReducer };
+
+const weatherReducer2 = (state = weatherReducerDefaultState, action: AppActions): Weather => {
+    switch (action.type) {
+        case Actions.GET_WEATHER_SUCCESS:
+            const weather = action.payload;
+            return weather;
+
+        case Actions.GET_WEATHER_FAIL:
+            return {
+                error: action.error
+            } as Weather;
+
+        default:
+            return state;
+    }
+};
+export { weatherReducer2 };
