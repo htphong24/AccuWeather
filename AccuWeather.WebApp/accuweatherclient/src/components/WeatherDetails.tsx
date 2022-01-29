35530dbedf9288c537ed6bc71f8bef4b784600e9
htphong24 <htphong24@gmail.com>
2022-01-28 23:16:30 -0800

    Updated WeatherDetails.tsx to add an additional city

--- AccuWeather.WebApp/accuweatherclient/src/components/WeatherDetails.tsx
+++ AccuWeather.WebApp/accuweatherclient/src/components/WeatherDetails.tsx
@@ -5,11 +5,13 @@ import { Weather } from '../types/Weather'
 
 interface IProp {
     weather: Weather,
+    weather2: Weather,
 };
 
 class WeatherDetails extends React.Component<IProp> {
     render() {
         const weather = this.props.weather;
+        const weather2 = this.props.weather2;
         return (
             <div>
                 <div className="city col-sm-9">
@@ -47,10 +49,51 @@ class WeatherDetails extends React.Component<IProp> {
                             </div>
                         </div>
                     }
+
+                    {
+                        weather2.location && <div>
+                            <h1>{weather2.location}</h1>
+                            <div className="row">
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                {
+                                                    weather2.weatherIcon && <img src={weather2.weatherIcon} className="img-thumbnail" />
+                                                }
+                                            </td>
+                                            <td>
+                                                <span>{weather2.weatherText}</span>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                {weather2.isDaytime && <span>
+                                                    Daytime
+                                                </span>}
+                                                {!weather2.isDaytime && <span>
+                                                    Night
+                                                </span>}
+                                            </td>
+                                            <td>
+                                                <span>{weather2.temperatureValue}&deg;{weather2.temperatureUnit}</span>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    }
+
                 </div>
                 {
                     weather.error && <p className="weatherError">
                         {weather.error}
+                    </p>
+                }
+                {
+                    weather2.error && <p className="weatherError">
+                        {weather2.error}
                     </p>
                 }
             </div>
@@ -60,7 +103,8 @@ class WeatherDetails extends React.Component<IProp> {
 
 
 const mapStateToProps = (state: AppState): IProp => ({
-    weather: state.weather
+    weather: state.weather,
+    weather2: state.weather2
 });
 
 export default connect(mapStateToProps)(WeatherDetails)
